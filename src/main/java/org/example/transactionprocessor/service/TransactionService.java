69fa1bb8a5227ecfa7d2659547b971b1287e79d9
alenaer <alana.eremina@gmail.com>
2025-02-12 17:55:55 +0100

    lombok added

--- src/main/java/org/example/transactionprocessor/service/TransactionService.java
+++ src/main/java/org/example/transactionprocessor/service/TransactionService.java
@@ -1,6 +1,7 @@
 package org.example.transactionprocessor.service;
 
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.example.transactionprocessor.model.Transaction;
 import org.example.transactionprocessor.repository.TransactionRepository;
 import org.springframework.scheduling.annotation.Async;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+@Slf4j
 @Service
 public class TransactionService {
 
@@ -21,24 +23,33 @@ public class TransactionService {
     }
 
     public List<Transaction> getAllTransactions() {
-        return transactionRepository.findAll();
+        log.info("Fetching all transactions from the database");
+        List<Transaction> transactions = transactionRepository.findAll();
+        log.info("Retrieved {} transactions", transactions.size());
+        return transactions;
     }
 
     //TODO Add validation,additional logic..
     @Async
     public CompletableFuture<Transaction> createTransaction(Transaction transaction) {
-        Transaction saveTransaction = transactionProcessor.processTransaction(transaction);
-        return CompletableFuture.completedFuture(saveTransaction);
+        log.info("Received request to create transaction: {}", transaction);
+        Transaction savedTransaction = transactionProcessor.processTransaction(transaction);
+        log.info("Transaction processed and saved: {}", savedTransaction);
+        return CompletableFuture.completedFuture(savedTransaction);
     }
 
     @Transactional
     public List<Transaction> createTransactionsBatch(List<Transaction> transactions) {
+        log.info("Processing batch of {} transactions", transactions.size());
         for (Transaction transaction : transactions) {
             if (transaction.getAccountFrom() == null || transaction.getAccountTo() == null) {
+                log.error("Invalid transaction data: {}", transaction);
                 throw new IllegalArgumentException("Invalid transaction data");
             }
         }
-        List<Transaction> savedTransactions = transactionProcessor.processTransactionsBatch(transactions);
-        return transactionRepository.saveAll(savedTransactions);
+        List<Transaction> processedTransactions = transactionProcessor.processTransactionsBatch(transactions);
+        List<Transaction> savedTransactions = transactionRepository.saveAll(processedTransactions);
+        log.info("Successfully processed and saved batch of {} transactions", savedTransactions.size());
+        return savedTransactions;
     }
 }
