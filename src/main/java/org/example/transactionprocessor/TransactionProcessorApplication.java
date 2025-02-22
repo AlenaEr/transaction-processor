be4d68289c2ba2931eb7a76920158e3b5e2462d4
alenaer <alana.eremina@gmail.com>
2025-02-22 23:52:13 +0100

    metrics added

--- src/main/java/org/example/transactionprocessor/TransactionProcessorApplication.java
+++ src/main/java/org/example/transactionprocessor/TransactionProcessorApplication.java
@@ -1,13 +1,23 @@
 package org.example.transactionprocessor;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.metrics.MetricsEndpoint;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TransactionProcessorApplication {
 
+    @Autowired
+    private MetricsEndpoint metricsEndpoint;
+
     public static void main(String[] args) {
         SpringApplication.run(TransactionProcessorApplication.class, args);
     }
 
+    @PostConstruct
+    public void init() {
+        System.out.println("Metrics Endpoint: " + metricsEndpoint);
+    }
 }
